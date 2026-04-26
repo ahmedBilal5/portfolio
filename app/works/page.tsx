38c3182d57748a0eeef826fd6a8685acd761eb4d
Ahmed Bilal <ahmed.bilal@zones.com>
2026-04-26 17:59:09 +0500

    missed in last commit

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -2,12 +2,13 @@ import BlogPostCard from "../_components/blogPostCard";
 import WorkCard from "../_components/workCard";
 import { featuredWorks, recentPosts } from "../constants";
 
-export default function Works(){
-    return <>
-    <h1 className="dark:text-white text-2xl sm:text-3xl md:text-4xl font-extrabold leading-tight sm:leading-[70px] text-center md:text-left my-5">
-              Works
-    </h1>
-    {featuredWorks.length > 0 ? (
+export default function Works() {
+  return (
+    <>
+      <h1 className="dark:text-white text-3xl md:text-4xl font-extrabold leading-tight sm:leading-[70px] text-left my-5">
+        Works
+      </h1>
+      {featuredWorks.length > 0 ? (
         <div className="grid grid-cols-1">
           {featuredWorks.map((work) => (
             <WorkCard
@@ -18,13 +19,14 @@ export default function Works(){
               tags={work.tags}
               imageUrl={work.imageUrl}
               href={work.href}
-              showOutline
+              showOutline={{ show: true, mb: "none" }}
+              extraPadY
             />
           ))}
         </div>
       ) : (
         <div
-            className="
+          className="
             text-center 
             py-12 
             text-slate-500 
@@ -35,4 +37,5 @@ export default function Works(){
         </div>
       )}
     </>
+  );
 }
