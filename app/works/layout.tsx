d87f92cf15aa8bb4fbdfe80ce37facbfe0f9ff2c
Ahmed Bilal <ahmedbilal2015@gmail.com>
2026-06-25 16:18:36 +0500

    fix: fixes except posts

--- app/works/layout.tsx
+++ app/works/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function WorksLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="md:max-w-[80%] mx-auto flex flex-col h-screen">
+    <div className="md:max-w-[80%] mx-auto flex flex-col min-h-screen">
       {children}
     </div>
   );
